97de0e08e252535c3666386d7a2deb7bdfd65902
e-a-s-t <erik@tallkronan.com>
2026-06-22 22:41:53 +0200

    Fix indentation for on_linux block in ctop.rb

--- Formula/ctop.rb
+++ Formula/ctop.rb
@@ -8,8 +8,10 @@ class Ctop < Formula
       url "https://github.com/e-a-s-t/ctop/releases/download/v0.2.10/ctop-darwin-arm64.tar.gz"
       sha256 "8a7403517db0f177dfe20e8b8f5c263e023e02f0ea14f66fd5d363c8619146ca"
     end
+  end
 
-    on_linux do
+  on_linux do
+    on_intel do
       url "https://github.com/e-a-s-t/ctop/releases/download/v0.2.10/ctop-linux-amd64.tar.gz"
       sha256 "41b0b4553f583cd760713ab72c96f4f23576644df608e238e6a689e0219c8931"
     end
