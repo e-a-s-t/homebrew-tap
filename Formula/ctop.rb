abd64de7d69a3a2c40448798df60cb2ad4160319
e-a-s-t <erik@tallkronan.com>
2026-06-22 17:03:16 +0200

    Update ctop version to 0.2.9 and URLs

--- Formula/ctop.rb
+++ Formula/ctop.rb
@@ -1,16 +1,16 @@
 class Ctop < Formula
   desc "top-like monitor for Codex and GitHub Copilot usage"
   homepage "https://github.com/e-a-s-t/ctop"
-  version "0.1.0"
+  version "0.2.9"
 
   on_macos do
     on_arm do
-      url "https://github.com/e-a-s-t/ctop/releases/download/v0.1.0/ctop-darwin-arm64.tar.gz"
+      url "https://github.com/e-a-s-t/ctop/releases/download/v0.2.9/ctop-darwin-arm64.tar.gz"
       sha256 "REPLACE_WITH_SHA256"
     end
 
     on_intel do
-      url "https://github.com/e-a-s-t/ctop/releases/download/v0.1.0/ctop-darwin-amd64.tar.gz"
+      url "https://github.com/e-a-s-t/ctop/releases/download/v0.2.9/ctop-darwin-amd64.tar.gz"
       sha256 "REPLACE_WITH_SHA256"
     end
   end
