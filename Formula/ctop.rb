2d2f1d88604fe1a20a4137e29434860a00036bff
e-a-s-t <erik@tallkronan.com>
2026-06-24 10:32:31 +0200

    Update ctop to version 0.2.12

--- Formula/ctop.rb
+++ Formula/ctop.rb
@@ -1,19 +1,19 @@
 class Ctop < Formula
   desc "top-like monitor for Codex and GitHub Copilot usage"
   homepage "https://github.com/e-a-s-t/ctop"
-  version "0.2.11"
+  version "0.2.12"
 
   on_macos do
     on_arm do
-      url "https://github.com/e-a-s-t/ctop/releases/download/v0.2.11/ctop-darwin-arm64.tar.gz"
-      sha256 "16df147b9aee69a57efd1fbb6716a67d9326723104c69c639fc111c86ed52727"
+      url "https://github.com/e-a-s-t/ctop/releases/download/v0.2.12/ctop-darwin-arm64.tar.gz"
+      sha256 "4f0eec3cdd80f25b143fe30f7e3f1b45e2f4ed9dc93fbdcc0a3cf6a6d951795a"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/e-a-s-t/ctop/releases/download/v0.2.11/ctop-linux-amd64.tar.gz"
-      sha256 "16df147b9aee69a57efd1fbb6716a67d9326723104c69c639fc111c86ed52727"
+      url "https://github.com/e-a-s-t/ctop/releases/download/v0.2.12/ctop-linux-amd64.tar.gz"
+      sha256 "4f0eec3cdd80f25b143fe30f7e3f1b45e2f4ed9dc93fbdcc0a3cf6a6d951795a"
     end
   end
 
