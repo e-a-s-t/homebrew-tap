e06e65c05ff8668218aacb2992cfa3d664b42e49
e-a-s-t <erik@tallkronan.com>
2026-06-27 15:47:28 +0200

    Update download URLs for lore package

--- Formula/lore.rb
+++ Formula/lore.rb
@@ -5,13 +5,13 @@ class Lore < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/e-a-s-t/lore-tui/releases/download/v0.1.5/lore-darwin-arm64.tar.gz"
+      url "https://github.com/e-a-s-t/lore/releases/download/v0.1.5/lore-darwin-arm64.tar.gz"
       sha256 "909a1c4a010460658c85e4304d93089bfae51d7ff2dcf5f93c55036941b15c00"
     end
   end
 
   on_linux do
-    url "https://github.com/e-a-s-t/lore-tui/releases/download/v0.1.5/lore-linux-amd64.tar.gz"
+    url "https://github.com/e-a-s-t/lore/releases/download/v0.1.5/lore-linux-amd64.tar.gz"
     sha256 "f5ccbd5e519e8975228e0436a7734b1ab170e0431417f450f5a220cfb98ef82b"
   end
 
